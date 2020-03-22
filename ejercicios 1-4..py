4a47bdd6bac5d2ef424273e708a16f87cb93e302
Vilma-09 <62492089+Vilma-09@users.noreply.github.com>
2020-03-22 13:10:42 -0600

    Add files via upload

---
+++ ejercicios 1-4..py
@@ -0,0 +1,17 @@
+print("ejercicio 1")
+for i in range (1,7):
+    print("*"*i)
+
+print("ejercicio 2")
+for e in range (6,0,-1):
+    print("*"*e)
+
+print("ejercicio 3")
+for i in range (1,8):
+    print("*"*i)
+for i in range (6,0,-1):
+    print("*"*i)
+
+print("ejercicio 4")
+for n in range(0,6):
+    print("*"*6)
